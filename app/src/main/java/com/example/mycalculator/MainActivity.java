98058d87ddd82c9937e9c865ab37a41b34ebe128
chukun09 <57821025+chukun09@users.noreply.github.com>
2020-10-14 11:11:35 +0700

    update2

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, add, sub, mul, div, addsub, equal, bs, dot, ce, c;
-    TextView tv1, tv2;
+    TextView tv1;
     Double var1, var2, ans;
     Boolean addition = false, subtract = false, multiply = false, divide = false;
 
@@ -39,65 +39,94 @@ public class MainActivity extends AppCompatActivity {
         dot = findViewById(R.id.btnDot);
 
         tv1 = findViewById(R.id.tvMath);
-        tv2 = findViewById(R.id.tvResult);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("1");
+                }else
                 tv1.setText(tv1.getText() + "1");
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("2");
+                }else
                 tv1.setText(tv1.getText() + "2");
             }
         });
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("3");
+                }else
                 tv1.setText(tv1.getText() + "3");
             }
         });
         b4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("4");
+                }else
                 tv1.setText(tv1.getText() + "4");
             }
         });
         b5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("5");
+                }else
                 tv1.setText(tv1.getText() + "5");
             }
         });
         b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("6");
+                }else
                 tv1.setText(tv1.getText() + "6");
             }
         });
         b7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("7");
+                }else
                 tv1.setText(tv1.getText() + "7");
             }
         });
         b8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("8");
+                }else
                 tv1.setText(tv1.getText() + "8");
             }
         });
         b9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("9");
+                }else
                 tv1.setText(tv1.getText() + "9");
             }
         });
         b0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(tv1.getText().toString().equals("0")){
+                    tv1.setText("0");
+                }else
                 tv1.setText(tv1.getText() + "0");
             }
         });
@@ -105,10 +134,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (tv1.getText().toString().equals("")) {
-
+                        tv1.setText("0");
                 } else {
                     String s = tv1.getText().toString();
                     s = s.substring(0, s.length() - 1);
+                    if(tv1.getText().toString().equals("0")){
+
+                    }else
                     tv1.setText(s);
                 }
             }
@@ -126,8 +158,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 setVar1();
-                buttonFalse();
                 addition = true;
+                tv1.setText("0");
             }
         });
 
@@ -136,8 +168,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 setVar1();
-                buttonFalse();
                 subtract = true;
+                tv1.setText("0");
             }
         });
 
@@ -146,8 +178,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 setVar1();
-                buttonFalse();
                 multiply = true;
+                tv1.setText("0");
             }
         });
 
@@ -156,42 +188,46 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 setVar1();
-                buttonFalse();
                 divide = true;
+                tv1.setText("0");
             }
         });
         ce.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv1.setText("");
+                tv1.setText("0");
             }
         });
         equal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ans = var1;
                 var2 = Double.parseDouble(tv1.getText().toString());
                 if(addition){
                     ans = var1 + var2;
+                    addition = false;
                 } else if (subtract){
                     ans = var1 - var2;
+                    subtract = false;
                 } else if (multiply){
                     ans = var1 * var2;
+                    multiply = false;
                 } else if (divide){
                     ans = var1 / var2;
+                    divide = false;
                 } else {
                     ans = var2;
                 }
 
-                tv2.setText(ans.toString());
-                equal.setEnabled(false);
+                tv1.setText(ans.toString());
+
 
             }
         });
         c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv1.setText("");
-                tv2.setText("");
+                tv1.setText("0");
                 equal.setEnabled(true);
                 sub.setEnabled(true);
                 mul.setEnabled(true);
@@ -200,14 +236,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-        public void buttonFalse(){
-
-            sub.setEnabled(false);
-            mul.setEnabled(false);
-            div.setEnabled(false);
-            add.setEnabled(false);
-            tv1.setText("");
-        }
+//        public void buttonFalse(){
+//
+//            sub.setEnabled(false);
+//            mul.setEnabled(false);
+//            div.setEnabled(false);
+//            add.setEnabled(false);
+//            tv1.setText("0");
+//        }
     public void setVar1(){
         var1 = Double.parseDouble(tv1.getText().toString());
     }
